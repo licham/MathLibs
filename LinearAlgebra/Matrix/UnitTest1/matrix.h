8ddb076104c7af04bf82bf1e8a1018905af9374b
licham <silaevmov@gmail.com>
2017-11-09 15:55:40 +0200

    Created matrix.h and some tests

--- LinearAlgebra/Matrix/UnitTest1/matrix.h
+++ LinearAlgebra/Matrix/UnitTest1/matrix.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 namespace ml {
 
@@ -9,53 +10,80 @@ namespace ml {
 
 	public:
 		Matrix();
-		Matrix(const Matrix &other);
-		Matrix(const std::vector<T> &vector);
-		Matrix(const size_t &rows, const size_t &cols);
+
 		Matrix(const std::pair<size_t, size_t> &size);
-		Matrix(const std::vector<std::vector<T>> &matrix);
+		Matrix(const std::pair<size_t, size_t> &size, const T & value);
+		Matrix(const size_t &rows, const size_t &cols);
 		Matrix(const size_t &rows, const size_t &cols, const T &value);
 
+		Matrix(const std::vector<T> &vector);
+		Matrix(const std::vector<std::vector<T>> &matrix);
+
+		Matrix(const Matrix &other);
+
 		void						setRow(const size_t &row, const std::vector<T> &vector);
+		void						setRow(const size_t &row, const T &value);
 		void						setCol(const size_t &col, const std::vector<T> &vector);
+		void						setCol(const size_t &col, const T &value);
+		void						addRow(const std::vector<T> &vector);
+		void						addCol(const std::vector<T> &vector);
 
-		std::pair<size_t, size_t>	getSize();
-		std::vector<T>				getRow(const size_t &row);
-		std::vector<T>				getCol(const size_t &col);
-		Matrix<T>					getMinor(const size_t &row, const size_t &col);
-		Matrix<T>					getInverse();
-		Matrix<T>					getTranspose();
+		const std::string			toString();
+
+		std::pair<size_t, size_t>	getSize() const;
+
+		std::vector<T>				getRow(const size_t &row) const;
+		std::vector<T>				getCol(const size_t &col) const;
+
+		Matrix<T>					getMinor(const size_t &row, const size_t &col) const;
+		Matrix<T>					getInverse() const;
+		Matrix<T>					getTranspose() const;
+
 		T							getDeterminant();
-
+		
 		std::vector<T>				eraseRow(const size_t &row);
 		std::vector<T>				eraseCol(const size_t &col);
 
 		std::vector<T>&				operator[](const size_t &row);
-		Matrix<T>					operator*(const T &value);
-		Matrix<T>					operator*(const Matrix<T> &other);
-		Matrix<T>					operator*(const std::vector<T> &vector);
-		Matrix<T>					operator*(const std::vector<std::vector<T>> &matrix);
-		Matrix<T>					operator/(const T &value);
-		Matrix<T>					operator/(const Matrix<T> &other);
-		Matrix<T>					operator/(const std::vector<std::vector<T>> &matrix);
-		Matrix<T>					operator+(const T &value);
-		Matrix<T>					operator+(const Matrix<T> &other);
-		Matrix<T>					operator+(const std::vector<std::vector<T>> &matrix);
-		Matrix<T>					operator-(const T &value);
-		Matrix<T>					operator-(const Matrix<T> &other);
-		Matrix<T>					operator-(const std::vector<std::vector<T>> &matrix);
-		bool						operator==(const Matrix<T> &other);
+
+		template<typename T> friend Matrix<T>			operator*(const Matrix<T> &left, const T &right);
+		template<typename T> friend Matrix<T>			operator*(const Matrix<T> &left, const Matrix<T> &right);
+		template<typename T> friend Matrix<T>			operator*(const Matrix<T> &left, const std::vector<T> &right);
+		template<typename T> friend Matrix<T>			operator*(const Matrix<T> &left, const std::vector<std::vector<T>> &right);
+		template<typename T> friend Matrix<T>			operator/(const Matrix<T> &left, const T &right);
+		template<typename T> friend Matrix<T>			operator/(const Matrix<T> &left, const Matrix<T> &right);
+		template<typename T> friend Matrix<T>			operator/(const Matrix<T> &left, const std::vector<std::vector<T>> &right);
+		template<typename T> friend Matrix<T>			operator+(const Matrix<T> &left, const T &right);
+		template<typename T> friend Matrix<T>			operator+(const Matrix<T> &left, const Matrix<T> &right);
+		template<typename T> friend Matrix<T>			operator+(const Matrix<T> &left, const std::vector<std::vector<T>> &right);
+		template<typename T> friend Matrix<T>			operator-(const Matrix<T> &left, const T &right);
+		template<typename T> friend Matrix<T>			operator-(const Matrix<T> &left, const Matrix<T> &right);
+		template<typename T> friend Matrix<T>			operator-(const Matrix<T> &left, const std::vector<std::vector<T>> &right);
+		template<typename T> friend bool				operator==(const Matrix<T> &left, const Matrix<T> &right);
 
 	private:
 		std::vector<std::vector<T>> matrix;
 		std::pair<size_t, size_t>	size;
+		bool						is_det;
 		T							det;
-		bool						det_calc;
 	};
 
 	template<typename T>
 	inline Matrix<T>::Matrix() {
-		det_calc = false;
+		is_det = false;
+		size = std::make_pair((size_t)0, (size_t)0);
+	}
+
+	template<typename T>
+	inline Matrix<T>::Matrix(const std::pair<size_t, size_t>& size) : Matrix<T>() {
+		this->size = size;
+		matrix = std::vector<std::vector<T>>::vector(size.first, std::vector<T>::vector(size.second, 0));
+	}
+
+	template<typename T>
+	inline Matrix<T>::Matrix(const std::pair<size_t, size_t>& size, const T & value) : Matrix<T>() {
+		this->size = size;
+		matrix = std::vector<std::vector<T>>::vector(size.first, std::vector<T>::vector(size.second, value));
 	}
 
 	template<typename T>
@@ -63,20 +91,29 @@ namespace ml {
 	}
 
 	template<typename T>
-	inline Matrix<T>::Matrix(const size_t & rows, const size_t & cols, const T & value) : Matrix<T>(std::make_pair(rows, cols)) {
-		for (auto &vec : matrix) {
-			for (auto &val : vec) {
-				val = value;
-			}
+	inline Matrix<T>::Matrix(const size_t & rows, const size_t & cols, const T & value) : Matrix<T>(std::make_pair(rows, cols), value) {
+	}
+
+	template<typename T>
+	inline Matrix<T>::Matrix(const std::vector<T>& vector) : Matrix<T>() {
+		if (vector.size() != 0) {
+			size = std::make_pair((size_t)vector.size(), (size_t)1);
+		}
+		for (size_t i = 0; i < size.first; ++i) {
+			matrix[i][0] = vector[i];
 		}
 	}
 
 	template<typename T>
-	inline Matrix<T>::Matrix(const std::pair<size_t, size_t>& size) : Matrix<T>() {
-		this->size = size;
-		matrix.resize(size.first);
-		for (auto &vec : matrix) {
-			vec.resize(size.second);
+	inline Matrix<T>::Matrix(const std::vector<std::vector<T>>& other) : Matrix<T>() {
+		if (other.size() != 0) {
+			size = std::make_pair(other.size(), other[0].size());
+		}
+		matrix = std::vector<std::vector<T>>::vector(size.first, std::vector<T>::vector(size.second));
+		for (size_t i = 0; i < size.first; ++i) {
+			for (size_t j = 0; j < size.second; ++j) {
+				matrix[i][j] = other[i][j];
+			}
 		}
 	}
 
@@ -84,26 +121,7 @@ namespace ml {
 	inline Matrix<T>::Matrix(const Matrix & other) : Matrix<T>(other.getSize()) {
 		for (size_t i = 0; i < size.first; ++i) {
 			for (size_t j = 0; j < size.second; ++j) {
-				matrix[i][j] = other[i][j];
-			}
-		}
-		det = other.getDeterminant();
-		det_calc = true;
-	}
-
-	template<typename T>
-	inline Matrix<T>::Matrix(const std::vector<T>& vector) : Matrix<T>(vector.size(), 1) {
-		for (size_t i = 0; i < size.first; ++i) {
-			matrix[i][0] = vector[i];
-		}
-	}
-
-	template<typename T>
-	inline Matrix<T>::Matrix(const std::vector<std::vector<T>>& matrix) : Matrix<T>() {
-		size = std::make_pair(matrix.size(), matrix[0].size());
-		for (size_t i = 0; i < size.first; ++i) {
-			for (size_t j = 0; j < size.second; ++j) {
-				this->matrix[i][j] = matrix[i][j];
+				matrix[i][j] = other.matrix[i][j];
 			}
 		}
 	}
@@ -113,7 +131,12 @@ namespace ml {
 		for (size_t i = 0; i < size.second; ++i) {
 			matrix[row][i] = vector[i];
 		}
-		det_calc = false;
+		is_det = false;
+	}
+
+	template<typename T>
+	inline void Matrix<T>::setRow(const size_t & row, const T & value) {
+		setRow(row, std::vector<T>::vector(size.second, value));
 	}
 
 	template<typename T>
@@ -121,21 +144,56 @@ namespace ml {
 		for (size_t i = 0; i < size.first; ++i) {
 			matrix[i][col] = vector[i];
 		}
-		det_calc = false;
+		is_det = false;
 	}
 
 	template<typename T>
-	inline std::pair<size_t, size_t> Matrix<T>::getSize() {
+	inline void Matrix<T>::setCol(const size_t & col, const T & value) {
+		setCol(col, std::vector<T>::vector(size.first, value));
+	}
+
+	template<typename T>
+	inline void Matrix<T>::addRow(const std::vector<T>& vector) {
+		is_det = false;
+		size.first++;
+		matrix.push_back(vector);
+	}
+
+	template<typename T>
+	inline void Matrix<T>::addCol(const std::vector<T>& vector) {
+		is_det = false;
+		size.second++;
+		for (size_t i = 0; i < size.first; ++i) {
+			matrix[i].push_back(vector[i]);
+		}
+	}
+
+	template<typename T>
+	inline const std::string Matrix<T>::toString() {
+		std::string res;
+		res += std::to_string(size.first) + ' ' + std::to_string(size.second) + '\n';
+		for (size_t i = 0; i < size.first; ++i) {
+			for (size_t j = 0; j < size.second; ++j) {
+				res += std::to_string(matrix[i][j]) + ' ';
+			}
+			res += '\n';
+		}
+		res += '\n';
+		return res;
+	}
+
+	template<typename T>
+	inline std::pair<size_t, size_t> Matrix<T>::getSize() const {
 		return size;
 	}
 
 	template<typename T>
-	inline std::vector<T> Matrix<T>::getRow(const size_t & row) {
+	inline std::vector<T> Matrix<T>::getRow(const size_t & row) const {
 		return matrix[row];
 	}
 
 	template<typename T>
-	inline std::vector<T> Matrix<T>::getCol(const size_t & col) {
+	inline std::vector<T> Matrix<T>::getCol(const size_t & col) const {
 		std::vector<T> temp(size.first);
 		for (size_t i = 0; i < size.first; ++i) {
 			temp[i] = matrix[i][col];
@@ -145,18 +203,19 @@ namespace ml {
 
 	template<typename T>
 	inline Matrix<T> Matrix<T>::getMinor(const size_t & row, const size_t & col) const {
-		Matrix<T> matrix(*this);
-		matrix.eraseRow(row);
-		matrix.eraseCol(col);
-		return matrix;
+		Matrix<T> temp(*this);
+		temp.eraseRow(row);
+		temp.eraseCol(col);
+		return temp;
 	}
 
 	template<typename T>
 	inline Matrix<T> Matrix<T>::getInverse() const {
-		Matrix<T> temp(size);
+		Matrix<T> temp(*this);
+		double t_det = temp.getDeterminant();
 		for (size_t i = 0; i < size.first; ++i) {
 			for (size_t j = 0; j < size.second; ++j) {
-				temp[i][j] = getMinor(j, i).getDeterminant() / det * ((i + j) % 2 == 0 ? 1 : -1);
+				temp[i][j] = getMinor(j, i).getDeterminant() / t_det * ((i + j) % 2 == 0 ? 1 : -1);
 			}
 		}
 		return temp;
@@ -175,16 +234,16 @@ namespace ml {
 
 	template<typename T>
 	inline T Matrix<T>::getDeterminant() {
-		if (det_calc) {
-			return determinant;
+		if (is_det) {
+			return det;
 		}
 		if (size.first == 1) return matrix[0][0];
 		double res = 0;
 		for (size_t i = 0; i < size.first; ++i) {
 			res += matrix[i][0] * getMinor(i, 0).getDeterminant() * (i % 2 == 0 ? 1 : -1);
 		}
-		determinant = res;
-		det_calc = true;
+		det = res;
+		is_det = true;
 		return res;
 	}
 
@@ -192,8 +251,8 @@ namespace ml {
 	inline std::vector<T> Matrix<T>::eraseRow(const size_t & row) {
 		std::vector<T> temp = matrix[row];
 		matrix.erase(matrix.begin() + row);
-		det_calc = false;
-		--size.second;
+		is_det = false;
+		--size.first;
 		return temp;
 	}
 
@@ -204,34 +263,34 @@ namespace ml {
 			temp[i] = matrix[i][col];
 			matrix[i].erase(matrix[i].begin() + col);
 		}
-		det_calc = false;
-		--size.first;
+		is_det = false;
+		--size.second;
 		return temp;
 	}
 
 	template<typename T>
 	inline std::vector<T>& Matrix<T>::operator[](const size_t & row) {
-		return &matrix[row];
+		return matrix[row];
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator*(const T & value) const {
-		Matrix<T> temp(*this);
-		for (auto &vec : temp) {
-			for (auto &val : vec) {
-				val *= value;
+	inline Matrix<T> operator*(const Matrix<T> &left, const T &right) {
+		Matrix<T> temp(left);
+		for (size_t i = 0; i < temp.getSize().first; ++i) {
+			for (size_t j = 0; j < temp.getSize().second; ++j) {
+				temp[i][j] *= right;
 			}
 		}
 		return temp;
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator*(const Matrix<T>& other) const {
-		Matrix<T> temp(size.first, other.getSize().second);
-		for (size_t i = 0; i < temp.getSize().first; i++) {
-			for (size_t j = 0; j < temp.getSize().second; j++) {
-				for (size_t k = 0; k < size.second; k++) {
-					temp[i][j] += matrix[i][k] * other[k][j];
+	inline Matrix<T> operator*(const Matrix<T> &left, const Matrix<T> &right) {
+		Matrix<T> temp(left.getSize().first, right.getSize().second);
+		for (size_t i = 0; i < temp.getSize().first; ++i) {
+			for (size_t j = 0; j < temp.getSize().second; ++j) {
+				for (size_t k = 0; k < left.getSize().second; ++k) {
+					temp[i][j] += left.matrix[i][k] * right.matrix[k][j];
 				}
 			}
 		}
@@ -239,98 +298,98 @@ namespace ml {
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator*(const std::vector<T>& vector) const {
-		return *this * Matrix<T>::Matrix(vector);
+	inline Matrix<T> operator*(const Matrix<T> &left, const std::vector<T> &right) {
+		return left * Matrix<T>::Matrix(right);
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator*(const std::vector<std::vector<T>>& matrix) const {
-		return *this * Matrix<T>::Matrix(matrix);
+	inline Matrix<T> operator*(const Matrix<T> &left, const std::vector<std::vector<T>> &right) {
+		return left * Matrix<T>::Matrix(right);
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator/(const T & value) const {
-		Matrix<T> temp(*this);
-		for (auto &vec : temp) {
-			for (auto &val : vec) {
-				val /= value;
+	inline Matrix<T> operator/(const Matrix<T> &left, const T &right) {
+		Matrix<T> temp(left);
+		for (size_t i = 0; i < temp.getSize().first; ++i) {
+			for (size_t j = 0; j < temp.getSize().second; ++j) {
+				temp[i][j] /= right;
 			}
 		}
 		return temp;
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator/(const Matrix<T>& other) const {
-		return *this * other.getInverse();
+	inline Matrix<T> operator/(const Matrix<T> &left, const Matrix<T> &right) {
+		return left * right.getInverse();
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator/(const std::vector<std::vector<T>>& matrix) const {
-		return *this / Matrix<T>::Matrix(matrix);
+	inline Matrix<T> operator/(const Matrix<T> &left, const std::vector<std::vector<T>> &right) {
+		return left / Matrix<T>::Matrix(right);
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator+(const T & value) const {
-		Matrix<T> temp(*this);
-		for (size_t i = 0; i < size.first; ++i) {
-			for (size_t j = 0; j < size.second; ++j) {
-				temp[i][j] += value;
+	inline Matrix<T> operator+(const Matrix<T> &left, const T &right) {
+		Matrix<T> temp(left);
+		for (size_t i = 0; i < temp.getSize().first; ++i) {
+			for (size_t j = 0; j < temp.getSize().second; ++j) {
+				temp[i][j] += right;
 			}
 		}
 		return temp;
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator+(const Matrix<T>& other) const {
-		Matrix<T> temp(size);
-		for (size_t i = 0; i < size.first; ++i) {
-			for (size_t j = 0; j < size.second; ++j) {
-				temp[i][j] = matrix[i][j] + other[i][j];
+	inline Matrix<T> operator+(const Matrix<T> &left, const Matrix<T> &right) {
+		Matrix<T> temp(left);
+		for (size_t i = 0; i < temp.getSize().first; ++i) {
+			for (size_t j = 0; j < temp.getSize().second; ++j) {
+				temp[i][j] += right.matrix[i][j];
 			}
 		}
 		return temp;
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator+(const std::vector<std::vector<T>>& matrix) const {
-		return *this + Matrix<T>::Matrix(matrix);
+	inline Matrix<T> operator+(const Matrix<T> &left, const std::vector<std::vector<T>> &right) {
+		return left + Matrix<T>::Matrix(right);
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator-(const T & value) const {
-		Matrix<T> temp(*this);
-		for (auto &vec : temp) {
-			for (auto &val : vec) {
-				val -= value;
+	inline Matrix<T> operator-(const Matrix<T> &left, const T &right) {
+		Matrix<T> temp(left);
+		for (size_t i = 0; i < temp.getSize().first; ++i) {
+			for (size_t j = 0; j < temp.getSize().second; ++j) {
+				temp[i][j] -= right;
 			}
 		}
 		return temp;
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator-(const Matrix<T>& other) const {
-		Matrix<T> temp(size);
-		for (size_t i = 0; i < size.first; ++i) {
-			for (size_t j = 0; j < size.second; ++j) {
-				temp[i][j] = matrix[i][j] - other[i][j];
+	inline Matrix<T> operator-(const Matrix<T> &left, const Matrix<T> &right) {
+		Matrix<T> temp(left);
+		for (size_t i = 0; i < temp.getSize().first; ++i) {
+			for (size_t j = 0; j < temp.getSize().second; ++j) {
+				temp[i][j] -= right.matrix[i][j];
 			}
 		}
 		return temp;
 	}
 
 	template<typename T>
-	inline Matrix<T> Matrix<T>::operator-(const std::vector<std::vector<T>>& matrix) const {
-		return *this - Matrix<T>::Matrix(matrix);
+	inline Matrix<T> operator-(const Matrix<T> &left, const std::vector<std::vector<T>> &right) {
+		return left - Matrix<T>::Matrix(right);
 	}
 
 	template<typename T>
-	inline bool	Matrix<T>::operator==(const Matrix<T> &other) const {
-		if (other.getSize() != size) {
+	inline bool	operator==(const Matrix<T> &left, const Matrix<T> &right) {
+		if (left.getSize() != right.getSize()) {
 			return false;
 		}
-		for (size_t i = 0; i < size.first; ++i) {
-			for (size_t j = 0; j < size.second; ++j) {
-				if (matrix[i][j] != other[i][j]) {
+		for (size_t i = 0; i < left.getSize().first; ++i) {
+			for (size_t j = 0; j < left.getSize().second; ++j) {
+				if (left.matrix[i][j] != right.matrix[i][j]) {
 					return false;
 				}
 			}
--- LinearAlgebra/Matrix/UnitTest1/unittest.cpp
+++ LinearAlgebra/Matrix/UnitTest1/unittest.cpp
@@ -1,30 +1,76 @@
 #include "stdafx.h"
-#include "CppUnitTest.h"
 #include "matrix.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace MatrixTest
-{		
+namespace MatrixTest {		
 
-	TEST_MODULE_INITIALIZE(ModuleInitialize) {
-		Logger::WriteMessage("In Module Initialize\n");
-	}
-
-	TEST_MODULE_CLEANUP(ModuleCleanup) {
-		Logger::WriteMessage("In Module Cleanup\n");
-	}
-
-	TEST_CLASS(MatrixTestClass)
-	{
+	TEST_CLASS(MatrixTestClass) {
 	public:
 		
-		TEST_METHOD(MatrixTestMethod1)
-		{
-			Logger::WriteMessage("In Method\n");
-			ml::Matrix<int> A(3, 3, 1);
-			ml::Matrix<int> B(3, 3, 2);
-			Assert::AreEqual(B, A + 1);
+		TEST_METHOD(Construct1) {
+			ml::Matrix<int> A(3, 5, 0);
+			std::vector<std::vector<int>> b = { {0, 0, 0, 0, 0},
+												{0, 0, 0, 0, 0},
+												{0, 0, 0, 0, 0} };
+			ml::Matrix<int> B(b);
+			ml::Matrix<int> C(B);
+			Assert::AreEqual(A == C, true);
+		}
+
+		TEST_METHOD(SetCol) {
+			ml::Matrix<int> A(3, 5, 0);
+			A.setCol(4, { 1, 2, 3 });
+			A.setCol(0, 4);
+			std::vector<std::vector<int>> b = { { 4, 0, 0, 0, 1 },
+												{ 4, 0, 0, 0, 2 },
+												{ 4, 0, 0, 0, 3 } };
+			ml::Matrix<int> B(b);
+			Assert::AreEqual(A == B, true);
+		}
+
+		TEST_METHOD(SetRow) {
+			ml::Matrix<int> A(3, 5, 0);
+			A.setRow(2, { 1, 2, 3, 4, 5 });
+			A.setRow(0, 6);
+			std::vector<std::vector<int>> b = { { 6, 6, 6, 6, 6 },
+												{ 0, 0, 0, 0, 0 },
+												{ 1, 2, 3, 4, 5 } };
+			ml::Matrix<int> B(b);
+			Assert::AreEqual(A == B, true);
+		}
+
+		TEST_METHOD(Divide) {
+			std::vector<std::vector<double>> a = {	{ 1, 2, 3 },
+													{ 4, 5, 6 },
+													{ 1, 6, 4 } };
+			ml::Matrix<double> A(a);
+			std::vector<std::vector<double>> b = {	{ 6, 7, 8 },
+													{ 1, 9, 3 },
+													{ 2, 5, 8 } };
+			ml::Matrix<double> B(b);
+			double det = 3. / 32;
+			Assert::AreEqual((A / B).getDeterminant(), det, 0.01);
+		}
+
+		TEST_METHOD(AddMethod) {
+			ml::Matrix<int> A((size_t)3, (size_t)3, 1),
+							B((size_t)3, (size_t)3, 2);
+			Assert::AreEqual((A + 1).toString(), B.toString());
+		}
+
+		TEST_METHOD(TransposeMethod) {
+			std::vector<std::vector<int>> a = { { 6, 6, 6, 6, 6 },
+												{ 0, 0, 0, 0, 0 },
+												{ 1, 2, 3, 4, 5 } };
+			ml::Matrix<int> A(a);
+			std::vector<std::vector<int>> b = { { 6, 0, 1 },
+												{ 6, 0, 2 },
+												{ 6, 0, 3 },
+												{ 6, 0, 4 },
+												{ 6, 0, 5 } };
+			ml::Matrix<int> B(b);
+			Assert::AreEqual(A.getTranspose() == B, true);
 		}
 
 	};
